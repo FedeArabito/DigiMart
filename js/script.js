85126281bde08559a82415e262e8292520a6f04b
Federico Arabito <federicoarabito1999@gmail.com>
2023-12-01 15:29:25 -0300

    bootstrap

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navi__menu");
+
+hamburger.addEventListener("click", () =>{
+  hamburger.classList.toggle("activo");
+  navMenu.classList.toggle("activo");
+
+})
+
+document.querySelectorAll(".navi-item", ".home").forEach(n => n.addEventListener("click", () =>{
+    hamburger.classList.remove("activo");
+    navMenu.classList.remove("activo");
+
+  }))
